ecfff20e39a41546d43d9c717bea1487dcdec3f1
PavithraR12 <149470085+PavithraR12@users.noreply.github.com>
2024-07-22 21:15:53 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,13 @@
+class variables
+{
+    public static void main(String a[])
+    {
+        System.out.println("ADDITION");
+        int num = 10;
+        int b = 10;
+        int c = num + b;
+        System.out.println(c);
+        System.out.println(num + b);
+        System.out.println(num);
+    }
+}
