c41e691f34739fa49d91f1087fbb9b55f7fe27b6
PavithraR12 <149470085+PavithraR12@users.noreply.github.com>
2024-07-22 21:17:52 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+public class add {
+    public static void main(String a[])
+    {
+        System.out.println("ADDITION OF NUMBERS");
+        int num1 = 100;
+        int num2 = 200;
+        int result = num1 + num2;
+        System.out.println(result);
+        System.out.println(2 + 2);
+    }
+    
+}
