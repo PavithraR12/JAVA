df131a494eab512ea72d9bee3c67305757cdc7bd
PavithraR12 <149470085+PavithraR12@users.noreply.github.com>
2024-07-22 21:19:23 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,9 @@
+class literals{
+    public static void main(String a[])
+    {
+        int num1 = 100;
+        int num2 = 101;
+        float num = 2e34f;
+        System.out.println(num);
+    }
+}
