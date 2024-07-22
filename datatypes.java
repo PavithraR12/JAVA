a5ebe1d77a84740daa18eeab45fae6ed6133c2b6
PavithraR12 <149470085+PavithraR12@users.noreply.github.com>
2024-07-22 21:18:47 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+public class datatypes
+{
+    public static void main(String a[])
+    {
+        int num1 = 5;
+        byte num2 = 127;
+        long num3 = 23456l;
+        short num4 = 23;
+        float num5 = 4.5f;
+        double num6 = 9.0;
+        char c = 'a';
+        boolean b = true;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
